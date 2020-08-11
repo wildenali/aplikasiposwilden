8edec8b745f99efe3aa7cb44f4bb168adea6fd98
wildenali <wildenwildenaliali@gmail.com>
2020-08-11 17:36:27 +0700

    Ulangi dari awal lagi

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Registrasi from './pages/registrasi';
-import Login from './pages/login';
-import NotFound from './pages/404';
 
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/registrasi" component={Registrasi}/>
-        <Route path="/login" component={Login}/>
-        <Route component={NotFound}/>
-      </Switch>
-    </Router>
+    <h1>Cek</h1>
   );
 }
 
