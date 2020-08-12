6ec5555bf3f0477cf2627891dfd915c20cdf49f6
wildenali <wildenwildenaliali@gmail.com>
2020-08-12 17:37:47 +0700

    cara menggunakan theme material-ui pada styles.js di registrasi page

--- src/pages/registrasi/index.js
+++ src/pages/registrasi/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import styles from './index.module.css'
 
 // import komponen material-ui
 import Button from '@material-ui/core/Button'
 
+// import styles
+import useStyles from './styles';
+
+
 function Registrasi() {
+
+  const classes = useStyles();
+
   return <>
-    <h1 className={styles.blue}>Halaman Registrasi</h1>
+    <h1 className={classes.blue}>Halaman Registrasi</h1>
     <Button color="primary" variant="contained">Click </Button>
   </>
 }
