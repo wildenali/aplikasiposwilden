b0324c5f7f6b01587f4efb27f597583717b7f0d8
wildenali <wildenwildenaliali@gmail.com>
2020-08-12 17:27:21 +0700

    cara menggunakan theme material-ui

--- src/App.js
+++ src/App.js
@@ -15,25 +15,29 @@ import FirebaseProvider from './components/FirebaseProvider';
 
 // import komponen material-ui
 import CssBaseline from '@material-ui/core/CssBaseline'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import theme from './config/theme';
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <FirebaseProvider>
-        <Router>
-          <Switch>
-            <PrivateRoute exact path="/" c omponent={Private} />
-            <PrivateRoute path="/pengaturan" component={Private} />
-            <PrivateRoute path="/produk" component={Private} />
-            <PrivateRoute path="/transaksi" component={Private} />
-            <Route path="/login" component={Login} />
-            <Route path="/registrasi" component={Registrasi} />
-            <Route path="/lupa-password" component={LupaPassword} />
-            <Route component={NotFound} />
-          </Switch>
-        </Router>
-      </FirebaseProvider>
+      <MuiThemeProvider theme={theme}>
+        <FirebaseProvider>
+          <Router>
+            <Switch>
+              <PrivateRoute exact path="/" c omponent={Private} />
+              <PrivateRoute path="/pengaturan" component={Private} />
+              <PrivateRoute path="/produk" component={Private} />
+              <PrivateRoute path="/transaksi" component={Private} />
+              <Route path="/login" component={Login} />
+              <Route path="/registrasi" component={Registrasi} />
+              <Route path="/lupa-password" component={LupaPassword} />
+              <Route component={NotFound} />
+            </Switch>
+          </Router>
+        </FirebaseProvider>
+      </MuiThemeProvider>
     </>
   );
 }
--- src/pages/registrasi/index.js
+++ src/pages/registrasi/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import styles from './index.module.css'
 
+// import komponen material-ui
+import Button from '@material-ui/core/Button'
+
 function Registrasi() {
-  return <h1 className={styles.blue}>Halaman Registrasi</h1>
+  return <>
+    <h1 className={styles.blue}>Halaman Registrasi</h1>
+    <Button color="primary" variant="contained">Click </Button>
+  </>
 }
 
 export default Registrasi;
