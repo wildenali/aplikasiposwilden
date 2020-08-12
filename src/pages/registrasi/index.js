08784ddb667c62d8f25f2287b550ba4cadf17a4f
wildenali <wildenwildenaliali@gmail.com>
2020-08-12 21:34:26 +0700

    Membuat registration page

--- src/pages/registrasi/index.js
+++ src/pages/registrasi/index.js
@@ -2,19 +2,85 @@ import React from 'react'
 
 // import komponen material-ui
 import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 
 // import styles
 import useStyles from './styles';
+import { Link } from 'react-router-dom';
 
 
 function Registrasi() {
 
   const classes = useStyles();
 
-  return <>
-    <h1 className={classes.blue}>Halaman Registrasi</h1>
-    <Button color="primary" variant="contained">Click </Button>
-  </>
+  return  <Container maxWidth="xs">
+            <Paper className={classes.paper}>
+              <Typography
+                variant="h5"
+                component="h1"
+                className={classes.title}
+              >
+                Buat Akun Baru
+              </Typography>
+              
+              <form>
+                <TextField
+                  id="email"
+                  type="email"
+                  name="email"
+                  margin="normal"
+                  label="Alamat Email"
+                  fullWidth
+                  required
+                />
+                <TextField
+                  id="password"
+                  type="password"
+                  name="password"
+                  margin="normal"
+                  label="Password"
+                  fullWidth
+                  required
+                />
+                <TextField
+                  id="ulangi_password"
+                  type="ulangi_password"
+                  name="ulangi_password"
+                  margin="normal"
+                  label="Ulangi Password"
+                  fullWidth
+                  required
+                />
+                <Grid container className={classes.buttons}>
+                  <Grid item xs>
+                    <Button
+                      type="submit"
+                      color="primary"
+                      variant="contained"
+                      size="large"
+                    >
+                      Daftar
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      component={Link}
+                      to="/login"
+                      variant="contained"
+                      size="large"
+                    >
+                      Login
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
+
+            </Paper>
+          </Container>
 }
 
 export default Registrasi;
--- src/pages/registrasi/styles.js
+++ src/pages/registrasi/styles.js
@@ -1,8 +1,17 @@
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles(theme => ({
-  blue: {
-    color: theme.palette.primary.main
+  title: {
+    // color: theme.palette.primary.main
+    textAlign: 'center',
+    marginBottom: theme.spacing(3)
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    padding: theme.spacing(6)
+  },
+  buttons:{
+    marginTop: theme.spacing(6)
   }
 }))
 
