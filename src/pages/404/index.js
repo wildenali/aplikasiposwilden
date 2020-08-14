73f85663634b1c22a2c3f7a7cf7cdb67de08af0c
wildenali <wildenwildenaliali@gmail.com>
2020-08-14 09:43:47 +0700

    Membuat NotFound_Page

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,7 +1,34 @@
 import React from 'react'
+ 
+// meterial-ui
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import { Link } from 'react-router-dom'
+
+// import styles
+import useStyles from './styles';
 
 function NotFound() {
-  return <h1>Halaman NotFound</h1>
+
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="xs">
+      <Paper className={classes.paper}>
+        <Typography variant="subtitle1" >
+          Halaman Tidak Ditemukan
+        </Typography>
+        <Typography variant="h3">
+          404
+        </Typography>
+        <Typography component={Link} to="/">
+          Kembali ke Beranda
+        </Typography>
+      </Paper>
+    </Container>
+  )
 }
 
 export default NotFound;
---
+++ src/pages/404/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+
+  paper: {
+    marginTop: theme.spacing(8),
+    padding: theme.spacing(6),
+    textAlign: 'center'
+  }
+
+}))
+
+export default useStyles;
