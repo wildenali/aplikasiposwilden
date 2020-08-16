e6606f42473d737ecd1465095cfac975616d7bdb
wildenali <wildenwildenaliali@gmail.com>
2020-08-16 13:02:22 +0700

    tambah Tab untuk pengguna dan toko di halaman pengaturan

--- src/pages/private/pengaturan/index.js
+++ src/pages/private/pengaturan/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+// metarial-ui
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Paper from '@material-ui/core/Paper';
+
 // komponen halaman pengguna
 import Pengguna from './pengguna';
 import Toko from './toko';
@@ -8,14 +13,20 @@ import Toko from './toko';
 
 function Pengaturan() {
   return (
-    <Switch>
-      <Route path="/pengaturan/pengguna" component={Pengguna} />
-      <Route path="/pengaturan/toko" component={Toko} />
+    <Paper>
+      <Tabs>
+        <Tab label="Pengguna" value='/pengaturan/pengguna' />
+        <Tab label="Toko" value='pengaturan/toko' />
+      </Tabs>
+      <Switch>
+        <Route path="/pengaturan/pengguna" component={Pengguna} />
+        <Route path="/pengaturan/toko" component={Toko} />
 
-      {/* Ini untuk mencegah jika ada org yg mau masuk ke /pengguna/apapunitugajelasasalaja nah 
-      bisa langsung di redirect ke sini, biar ga aneh2 si user/testerjahil nya */}
-      <Redirect to="/pengaturan/pengguna" />  
-    </Switch>
+        {/* Ini untuk mencegah jika ada org yg mau masuk ke /pengguna/apapunitugajelasasalaja nah 
+        bisa langsung di redirect ke sini, biar ga aneh2 si user/testerjahil nya */}
+        <Redirect to="/pengaturan/pengguna" />  
+      </Switch>
+    </Paper>
   )
 }
 
