ff2c72138d6e6d8ebf877ba35af3aacf87245f07
wildenali <wildenwildenaliali@gmail.com>
2020-08-17 15:04:55 +0700

    Menambahkan textfield pada pengaturan pengguna, untuk input nama pengguna app nya

--- src/pages/private/pengaturan/pengguna.js
+++ src/pages/private/pengaturan/pengguna.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+
+// material ui
+import TextField from '@material-ui/core/TextField'
+
+import { useFirebase } from '../../../components/FirebaseProvider'
 
 function Pengguna() {
-  return <h1>Halaman Pengaturan Pengguna</h1>
+
+  const { user } = useFirebase();
+  const [isSubmitting, setSubmitting] = useState(false)
+  const displayNameRef = useRef()
+
+  const saveDisplayName = async (e) => {
+    const displayName = displayNameRef.current.value;
+    console.log(displayName);
+
+    setSubmitting(true)
+    await user.updateProfile({
+      displayName
+    })
+    setSubmitting(false)
+
+  }
+
+  return  <>
+            <TextField
+              id="displayName"
+              name="displayName"
+              label="Name"
+              defaultValue={user.displayName}
+              inputProps={{
+                ref: displayNameRef,
+                onBlur: saveDisplayName
+              }}
+              disabled={isSubmitting}
+            />
+          </>
 }
 
 export default Pengguna;
