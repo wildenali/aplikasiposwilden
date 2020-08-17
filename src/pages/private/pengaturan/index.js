c380c7df17f68a5ec3b2af5ba303b29add3842cd
wildenali <wildenwildenaliali@gmail.com>
2020-08-17 14:20:18 +0700

    tambah style untuk halaman pengaturan pengguna dan pengaturan toko

--- src/pages/private/pengaturan/index.js
+++ src/pages/private/pengaturan/index.js
@@ -10,10 +10,13 @@ import Paper from '@material-ui/core/Paper';
 import Pengguna from './pengguna';
 import Toko from './toko';
 
+// import styles
+import useStyles from './styles'
 
 function Pengaturan(props) {
 
   const {location, history} = props;
+  const classes =  useStyles();
 
   const handleChangeTab = (event, value) => {
     history.push(value)
@@ -30,14 +33,16 @@ function Pengaturan(props) {
         <Tab label="Pengguna" value='/pengaturan/pengguna' />
         <Tab label="Toko" value='/pengaturan/toko' />
       </Tabs>
-      <Switch>
-        <Route path="/pengaturan/pengguna" component={Pengguna} />
-        <Route path="/pengaturan/toko" component={Toko} />
+      <div className={classes.tabContent}>
+        <Switch>
+          <Route path="/pengaturan/pengguna" component={Pengguna} />
+          <Route path="/pengaturan/toko" component={Toko} />
 
-        {/* Ini untuk mencegah jika ada org yg mau masuk ke /pengguna/apapunitugajelasasalaja nah 
-        bisa langsung di redirect ke sini, biar ga aneh2 si user/testerjahil nya */}
-        <Redirect to="/pengaturan/pengguna" />  
-      </Switch>
+          {/* Ini untuk mencegah jika ada org yg mau masuk ke /pengguna/apapunitugajelasasalaja nah 
+          bisa langsung di redirect ke sini, biar ga aneh2 si user/testerjahil nya */}
+          <Redirect to="/pengaturan/pengguna" />  
+        </Switch>
+      </div>
     </Paper>
   )
 }
