12259002e6607a28e7093814df48c65ac87fa676
wildenali <wildenwildenaliali@gmail.com>
2020-08-18 14:33:05 +0700

    posisi AddIcon di pindahin ke pojok kanan bawah

--- src/pages/private/produk/grid.js
+++ src/pages/private/produk/grid.js
@@ -4,10 +4,17 @@ import React from 'react'
 import Fab from '@material-ui/core/Fab'
 import AddIcon from '@material-ui/icons/Add'
 
+// import styles
+import useStyles from './styles/grid'
+
 function GridProduk() {
+
+  const classes = useStyles();
+
   return  <>
             <h1>Halaman GridProduk</h1>
             <Fab
+              className={classes.fab}
               color="primary"
             >
               <AddIcon />
