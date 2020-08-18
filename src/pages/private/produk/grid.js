503d48ad1f214bb3c16930e33fbd1f3bcfff6d84
wildenali <wildenwildenaliali@gmail.com>
2020-08-18 14:29:15 +0700

    add AddIcon in Halaman GridProduk

--- src/pages/private/produk/grid.js
+++ src/pages/private/produk/grid.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+// material-ui
+import Fab from '@material-ui/core/Fab'
+import AddIcon from '@material-ui/icons/Add'
+
 function GridProduk() {
-  return <h1>Halaman GridProduk</h1>
+  return  <>
+            <h1>Halaman GridProduk</h1>
+            <Fab
+              color="primary"
+            >
+              <AddIcon />
+            </Fab>
+          </>
 }
 
 export default GridProduk;
