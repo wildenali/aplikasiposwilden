b240309412313c208e10046c992c2d569a631360
wildenali <wildenwildenaliali@gmail.com>
2020-08-19 09:00:37 +0700

    Membuat grid produk

--- src/pages/private/produk/grid.js
+++ src/pages/private/produk/grid.js
@@ -3,16 +3,36 @@ import React, { useState } from 'react'
 // material-ui
 import Fab from '@material-ui/core/Fab'
 import AddIcon from '@material-ui/icons/Add'
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import Typography from '@material-ui/core/Typography'
 
 // import styles
 import useStyles from './styles/grid'
 import AddDialog from './add';
 
+import { useFirebase } from '../../../components/FirebaseProvider'
+import AppPageLoading from '../../../components/AppPageLoading'
+import { useCollection } from 'react-firebase-hooks/firestore'
+
 function GridProduk() {
 
   const classes = useStyles();
 
+  const {firestore, user} = useFirebase()
+
+  const produkCol = firestore.collection(`toko/${user.uid}/produk`)
+
+  const [snapshot, loading] = useCollection(produkCol)
+
   const [openAddDialog, setOpenAddDialog] = useState(false)
+
+  if (loading) {
+    return <AppPageLoading />
+  }
 
   return  <>
             <h1>Halaman GridProduk</h1>
