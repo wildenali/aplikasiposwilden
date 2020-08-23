3e8f2ce5438bb4118b8abb061b3b47e23753182e
wildenali <wildenwildenaliali@gmail.com>
2020-08-23 11:47:26 +0700

    membuat tabel untuk detail view page

--- src/pages/private/transaksi/details.js
+++ src/pages/private/transaksi/details.js
@@ -9,12 +9,57 @@ import DialogActions from '@material-ui/core/DialogActions'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 
+import Table from '@material-ui/core/Table'
+import TableRow from '@material-ui/core/TableRow'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+
+import { currency } from '../../../utils/formatter'
+
 function DetailsDialog({ open, handleClose, transaksi }) {
   return <Dialog
     open={open}
     onClose={handleClose}
   >
     <DialogTitle>Transaksi No: {transaksi.no}</DialogTitle>
+    <DialogContent>
+      <Table>
+        <TableHead>
+          <TableCell>Item</TableCell>
+          <TableCell>Jumlah</TableCell>
+          <TableCell>Harga</TableCell>
+          <TableCell>Subtotal</TableCell>
+        </TableHead>
+        <TableBody>
+          { 
+            transaksi.items && Object.keys(transaksi.items).map(k => {
+                const item = transaksi.items[k]
+                return (
+                  <TableRow key={k}>
+                    <TableCell>{item.nama}</TableCell>
+                    <TableCell>{item.jumlah}</TableCell>
+                    <TableCell>{currency(item.harga)}</TableCell>
+                    <TableCell>{currency(item.subtotal)}</TableCell>
+                  </TableRow>
+                )
+              })
+          }
+          <TableRow>
+            <TableCell colSpan={3}>
+              <Typography variant="subtitle2">
+                Total
+              </Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant="h6">
+                {currency(transaksi.total)}
+              </Typography>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </DialogContent>
   </Dialog>
 }
 
